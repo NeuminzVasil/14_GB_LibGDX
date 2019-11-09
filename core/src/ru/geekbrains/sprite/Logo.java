71a991b7e312ff33b72ee7b5887a8a470042af89
Laptop6440 <Zexa91x0@mail.ru>
2019-11-09 14:31:04 +0300

    Lesson 03 - field preparing

---
+++ core/src/ru/geekbrains/sprite/Logo.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.sprite;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import ru.geekbrains.base.Sprite;
+
+public class Logo extends Sprite {
+
+    public Logo(TextureRegion region) {
+        super(region);
+    }
+}
