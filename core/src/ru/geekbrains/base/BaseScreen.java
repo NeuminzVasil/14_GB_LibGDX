15f04a732d37a46b90ae30e6fb64d34117ef5ce6
Zexa91x0 <Zexa91x0@mail.ru>
2019-11-24 20:09:29 +0500

    Lesson 02: Разобрался с ДЗ

--- core/src/ru/geekbrains/base/BaseScreen.java
+++ core/src/ru/geekbrains/base/BaseScreen.java
@@ -30,34 +30,32 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public void render(float delta) {
-    // обязательно должен присутсвовать даже если пустой. неясно почему.
-    // работет 60 раз в секунду
+        // обязательно должен присутсвовать даже если пустой. неясно почему.
+        // работет 60 раз в секунду
     }
 
     @Override
     public void resize(int width, int height) {
-        System.out.println("resize width = " + width + " height = " + height);
     }
 
     @Override
     public void pause() {
-        System.out.println("pause");
+        System.err.println("BaseScreen.pause()");
     }
 
     @Override
     public void resume() {
-        System.out.println("resume");
+        System.err.println("BaseScreen.resume()");
     }
 
     @Override
     public void hide() {
-        System.out.println("hide");
         dispose();
     }
 
     @Override
     public void dispose() {
-        System.out.println("dispose");
+        System.err.println("BaseScreen.dispose()");
         // метод не вызывается по умолчанию
         // сделан для того чтобы разработчик вызывал его там где это действительно нужно .
         // обычно вызвается из метода Hide
@@ -65,37 +63,31 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public boolean keyDown(int keycode) {
-        System.out.println("keyDown keycode = " + keycode);
         return false;
     }
 
     @Override
     public boolean keyUp(int keycode) {
-        System.out.println("keyUp keycode = " + keycode);
         return false;
     }
 
     @Override
     public boolean keyTyped(char character) {
-        System.out.println("keyTyped character = " + character);
         return false;
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        System.out.println("touchDown screenX = " + screenX + " screenY = " + screenY);
         return false;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        System.out.println("touchUp screenX = " + screenX + " screenY = " + screenY);
         return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-        System.out.println("touchDragged screenX = " + screenX + " screenY = " + screenY);
         return false;
     }
 
@@ -106,7 +98,6 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public boolean scrolled(int amount) {
-        System.out.println("scrolled amount = " + amount);
         return false;
     }
 }
--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -14,23 +14,34 @@ import ru.geekbrains.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
-    private Texture img;
+    private Texture badLogicLogoTexture;
 
-    private Vector2 pos; // позиция
-    private Vector2 v; // скорость
+    private Vector2 currentPosition; // вектор (точка) текущей позиции позиции
+    private Vector2 nexStepVector; // вектор направления движения (следующего шага).
+    private Vector2 touchPoint; // вектор (точка) конечной позиции при клике на экране.
+    private static final float V_LEN = 5.5f; // переменная скорости, за счет нормализации длинны шага на ЭТУ величену
+    private Vector2 nexStepVectorTesting;
 
+    /**
+     * метод отрабатывающий при первом отображении окна.
+     * как правило используется для инициализации переменных.
+     */
     @Override
     public void show() {
         super.show();
-        // попробовал новый логотип
-        img = new Texture("NewBLLogo.jpg");
-        pos = new Vector2();
-        v = new Vector2(10f, 5.5f); // задаем скорость. Под скоростью понимем вектор смещения основной позиции.
+
+        badLogicLogoTexture = new Texture("NewBLLogo.jpg");// попробовал новый логотип
+
+        currentPosition = new Vector2(); // создаем объект вектора (точки) текущей позиции.
+        nexStepVector = new Vector2(); // создаем объект ветора скорости и направлени следующего шага).
+        touchPoint = new Vector2(); // создаем объект вектора конечной точки.
+        nexStepVectorTesting = new Vector2(); // вектор для провеки текущей позииции - не самое оптимальное решение . нужно сделать булин и проверять в отдельном методе чтобы не проводить вычислений 60 раз в секунду
     }
 
 
     /**
      * метод отрабатывающий 60 раз в секунду во время работы приложения.
+     *
      * @param delta - Дельта переменная как правило для реализации времени, задершки , погрешности и тд.
      */
     @Override
@@ -38,32 +49,56 @@ public class MenuScreen extends BaseScreen {
         super.render(delta);
 
         Gdx.gl.glClearColor(1, 1, 0.60f, 1); // цвет фона окна в RGB
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // не понял что это ??
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // не понял что это ?? - посмотреть в первом уроке. todo
 
         batch.begin(); // начало отрисовки ползущей картинки. Нарисовали ее в новой позиции. Не ясно почему первые позиции заданы 0?
-        batch.draw(img, // ползущая картинка
-                pos.x, pos.y // координаты ползущей картинки
+        batch.draw(badLogicLogoTexture, // ползущая картинка
+                currentPosition.x, currentPosition.y // текушее положение ползущей картинки
         );
-        batch.end(); // конец отрисовки ползущей картинки ?
+        batch.end(); // конец отрисовки ползущей картинки
 
-
-        if (Gdx.graphics.getHeight() >= pos.y + img.getHeight()
-                && Gdx.graphics.getWidth() >= pos.x + img.getWidth()) {
-            pos.add(v); // если картинка не выходит за рамки экрана то приваить к текущей позицици "вектор скорости"(читай координаты смещения)
+        nexStepVectorTesting.set(touchPoint); // копируем вектор конечной позиции чтобы не пложить объекты методом cpy()
+        // из вектора конечной позиции вычитаем вектор текущей позиции и смотрим длину волученного вектора
+        if (nexStepVectorTesting.sub(currentPosition).len() > V_LEN) { //если растояние между конечной и текушей точками больше длинны шага то делаем шаг
+            currentPosition.add(nexStepVector); // do the next step.
+            //System.out.println("MenuScreen.render(): ..sub.len() = " + nexStepVectorTesting.len() + "(" + currentPosition.x + "," + currentPosition.y + ")");
+        } else { // если меньше то это занчит что мы достигли конечной точки и нужно встать на заданную позицию.
+            currentPosition.set(touchPoint); // set finish.
+            //System.out.println("MenuScreen.render(): FINISH ..sub.len() = " + nexStepVectorTesting.len() + "(" + currentPosition.x + "," + currentPosition.y + ")");
         }
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        System.out.println("MenuScreen.keyDown(): keycode: " + keycode);
+        if (keycode >= 19 && keycode <= 22) {
+            currentPosition.set(0f, 0f);
+        }
+        return super.keyDown(keycode);
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+        badLogicLogoTexture.dispose();
         super.dispose();
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+
         super.touchDown(screenX, screenY, pointer, button);
-        System.out.println(screenX + "; " + (Gdx.graphics.getHeight() - screenY));
+
+        touchPoint.set(screenX, Gdx.graphics.getHeight() - screenY);
+        System.out.println("MenuScreen.touchDown(): touchPoint (Сюда кликнул мышкой: ): " + touchPoint.x + ", " + touchPoint.y);
+        System.out.println("MenuScreen.touchDown(): currentPosition (я здесь: ): " + currentPosition.x + ", " + currentPosition.y);
+        System.out.println("MenuScreen.touchDown(): touchPoint.cpy().sub(currentPosition): " + touchPoint.cpy().sub(currentPosition).x + ", " + touchPoint.cpy().sub(currentPosition).y);
+        System.out.println("MenuScreen.touchDown(): (touchPoint.cpy().sub(currentPosition)).nor(): " + (touchPoint.cpy().sub(currentPosition)).nor().x + ", " + (touchPoint.cpy().sub(currentPosition)).nor().y);
+
+        //nexStepVector.set(touchPoint.cpy().sub(currentPosition)).nor(); // получаем вектор смещения путем разницы между конечной и текущей позициями и полседующей нормализации вектора.
+        nexStepVector.set(touchPoint.cpy().sub(currentPosition)).setLength(V_LEN); // получаем вектор смещения путем разницы между конечной и текущей позициями  и полседующей корректировки длинны полученного вектора с помощью константы скорости
+        System.out.println("MenuScreen.touchDown(): nexStepVector (вектор точки следующего шага: ): " + nexStepVector.x + ", " + nexStepVector.y + "\n");
+
         return false;
     }
 }
